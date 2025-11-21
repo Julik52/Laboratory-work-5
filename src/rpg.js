c71d617ea0cd1c34ae6a16f7c32c544515a3e4e5
Parshikova Julia <145702408+Julik52@users.noreply.github.com>
2025-11-21 21:55:53 +0200

    Lab 5 completed

--- src/rpg.js
+++ src/rpg.js
@@ -1,9 +1,6 @@
 // src/rpg.js
 
-// --- МОДУЛЬНІ ФУНКЦІЇ (Чиста логіка) ---
-
 // 1. Розрахунок базового урону (Атака - Захист)
-// Якщо захист більший за атаку, урон = 0 (не може бути мінусовим)
 function calculateDamage(attack, defense) {
     if (attack <= defense) return 0;
     return attack - defense;
@@ -19,27 +16,24 @@ function getCharacterStatus(hp) {
     return hp > 0 ? "Alive" : "Dead";
 }
 
-// --- ІНТЕГРАЦІЙНІ ФУНКЦІЇ (Взаємодія) ---
-
 // 4. Проведення атаки (Інтегрує розрахунок урону та критичні удари)
 function performAttack(attacker, defender, diceRoll) {
     let damage = calculateDamage(attacker.attack, defender.defense);
-    
-    // Якщо крит - урон подвоюється
+
     if (isCriticalHit(diceRoll)) {
         damage *= 2;
     }
     
     return {
         damageDealt: damage,
-        isCritical: isCriticalHit(diceRoll) // Використовуємо модульну функцію
+        isCritical: isCriticalHit(diceRoll) 
     };
 }
 
 // 5. Оновлення стану після бою (Інтегрує нанесення урону та перевірку статусу)
 function applyDamage(defender, damage) {
     const newHp = defender.hp - damage;
-    const status = getCharacterStatus(newHp); // Використовуємо модульну функцію
+    const status = getCharacterStatus(newHp); 
     
     return {
         remainingHp: newHp,
